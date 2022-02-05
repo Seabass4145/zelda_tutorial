f011f7f74bd2e2502b18eb58cf5f98ed3e874833
Sebastian Pizzuto <spizzuto@uri.edu>
2022-02-05 12:29:09 -0500

    sprites

--- code/main.py
+++ code/main.py
@@ -9,7 +9,7 @@ class Game:
         # general setup
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
-        pygame.display.set_caption('Zelda')
+        pygame.display.set_caption('305 RPG')
         self.clock = pygame.time.Clock()
 
         self.level = Level()
