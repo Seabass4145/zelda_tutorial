3878cb48a7bb33e6d96fad7dfaa454346e60c91c
Sebastian Pizzuto <spizzuto@uri.edu>
2022-02-05 13:10:53 -0500

    added collision

--- code/player.py
+++ code/player.py
@@ -3,37 +3,59 @@ from settings import *
 
 
 class Player(pygame.sprite.Sprite):
-	def __init__(self, pos, groups):
-		super().__init__(groups)
-		self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
-		self.rect = self.image.get_rect(topleft=pos)
+    def __init__(self, pos, groups, obstacle_sprites):
+        super().__init__(groups)
+        self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft=pos)
 
-		self.direction = pygame.math.Vector2()
-		self.speed = 5
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
 
-	def input(self):
-		keys = pygame.key.get_pressed()
+        self.obstacle_sprites = obstacle_sprites
 
-		if keys[pygame.K_UP]:
-			self.direction.y = -1
-		elif keys[pygame.K_DOWN]:
-			self.direction.y = 1
-		else:
-			self.direction.y = 0
+    def input(self):
+        keys = pygame.key.get_pressed()
 
-		if keys[pygame.K_RIGHT]:
-			self.direction.x = 1
-		elif keys[pygame.K_LEFT]:
-			self.direction.x = -1
-		else:
-			self.direction.x = 0
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
 
-	def move(self, speed):
-		if self.direction.magnitude() != 0:
-			self.direction = self.direction.normalize()
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
 
-		self.rect.center += self.direction * speed
+    def move(self, speed):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
 
-	def update(self):
-		self.input()
-		self.move(self.speed)
+        self.rect.x += self.direction.x * speed
+        self.collision('horizontal')
+        self.rect.y += self.direction.y * speed
+        self.collision('vertical')
+
+    def collision(self, direction):
+        if direction == 'horizontal':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.x > 0:  # move right
+                        self.rect.right = sprite.rect.left
+                    if self.direction.x < 0:  # move left
+                        self.rect.left = sprite.rect.right
+
+        if direction == 'vertical':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.y > 0:  # move down
+                        self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0:  # move up
+                        self.rect.top = sprite.rect.bottom
+
+    def update(self):
+        self.input()
+        self.move(self.speed)
